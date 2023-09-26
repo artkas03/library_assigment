83fd1cc8ff0a900cc35e8898a41004263af5f690
artkas03 <zhonayer2017@gmail.com>
2023-09-26 20:59:55 +0300

    created server side

--- frontend/src/api/user.js
+++
@@ -1,7 +0,0 @@
-const { client } = require("../utils/client");
-
-export const authUser = async (data) => {
-  const response = await client.post('/authorization', data);
-
-  return response;
-};
--- frontend/src/utils/functions.js
+++
@@ -1,7 +0,0 @@
-export const createAnswerSheet = (test) => {
-  return test.quiz.reduce((acc, { question }) => {
-    acc[question] = 0;
-
-    return acc;
-  }, {})
-}
