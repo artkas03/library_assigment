4392730ca1094e3b212998e5870999b1caff2826
artkas03 <zhonayer2017@gmail.com>
2023-09-27 16:24:20 +0300

    added loader

--- src/pages/Tests/Tests.jsx
+++ src/pages/Tests/Tests.jsx
@@ -2,15 +2,18 @@ import React, { useEffect, useState } from 'react'
 import TestItem from './components/TestItem/TestItem';
 import { getAssigmentsByUserId, sendAssigmentAnswer } from '../../api/assigments';
 import Assigment from '../Assigment/Assigment';
+import { Loader } from '../../components/Loader/Loader';
 
 export default function Tests({ user }) {
+  const [isLoading, setIsLoading] = useState(true);
   const [assigments, setAssigments] = useState([]);
   const [currentAssigment, setCurrentAssigment] = useState(null);
 
   const loadAssigments = () => {
     getAssigmentsByUserId(user.id)
       .then(setAssigments)
-      .catch(console.log);
+      .catch(console.log)
+      .finally(() => setIsLoading(false));
   }
 
   useEffect(() => {
@@ -25,29 +28,35 @@ export default function Tests({ user }) {
   }
 
   return (
-    <div>
-      {currentAssigment ? (
-        <Assigment 
-          test={currentAssigment}
-          sendAnswers={sendAssigmentAnswers}
-        />
+    <>
+      {isLoading ? (
+        <Loader />
       ) : (
-        <div className="testlist">
-          {assigments.length && (
-            <>
-              {assigments.map(({ assignedTest, score, isCompleted }) => (
-                <TestItem
-                  isCompleted={isCompleted}
-                  score={score}
-                  key={assignedTest.id} 
-                  test={assignedTest}
-                  onAssigmentStart={setCurrentAssigment}
-                />
-              ))}
-            </>
-          )}
-        </div>
+      <div>
+        {currentAssigment ? (
+          <Assigment 
+            test={currentAssigment}
+            sendAnswers={sendAssigmentAnswers}
+          />
+        ) : (
+          <div className="testlist">
+            {assigments.length && (
+              <>
+                {assigments.map(({ assignedTest, score, isCompleted }) => (
+                  <TestItem
+                    isCompleted={isCompleted}
+                    score={score}
+                    key={assignedTest.id} 
+                    test={assignedTest}
+                    onAssigmentStart={setCurrentAssigment}
+                  />
+                ))}
+              </>
+            )}
+          </div>
+        )}
+      </div>
       )}
-    </div>
+    </>
   )
 }
