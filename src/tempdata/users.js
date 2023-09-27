a45025a991c03343bcd1101d6697ad2281396035
artkas03 <zhonayer2017@gmail.com>
2023-09-27 22:36:46 +0300

    fixed assigment creation issue

--- src/tempdata/users.js
+++ src/tempdata/users.js
@@ -1,10 +1,8 @@
 const User = require('../classes/User.js');
 const { getDefaultAssigments } = require('../utils/functions.js');
 
-const defaultAssigments = getDefaultAssigments();
-
-const testUser = new User('Artem', 'test', 'test', defaultAssigments);
-const testUser2 = new User('Georg', 'test2', 'test2', defaultAssigments);
+const testUser = new User('Artem', 'test', 'test', getDefaultAssigments());
+const testUser2 = new User('Georg', 'test2', 'test2', getDefaultAssigments());
 
 const users = [
   testUser,
